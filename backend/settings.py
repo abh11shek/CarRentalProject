a4e276d3bfc84bafd49f60761c4040b9e8ec16b4
Abhishek Anand <anand.abhishek1603@gmail.com>
2022-07-01 20:19:18 +0530

    Second commit

--- backend/settings.py
+++ backend/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
+import os
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -158,7 +161,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -170,6 +172,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
+STSTIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = 'static/images'
 
 # Default primary key field type
@@ -180,3 +183,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually
 CORS_ALLOW_ALL_ORIGINS = True
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
